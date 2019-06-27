21baaf2d14f17970e722720e62c1e46dccad03ac
cristii006 <andrei-cristian.simandan@nokia.com>
2019-06-27 11:08:52 +0300

    Add tests

--- atest/testdata/test_libraries/LibraryDecorator.py
+++ atest/testdata/test_libraries/LibraryDecorator.py
@@ -2,18 +2,15 @@ from robot.api.deco import keyword, library
 
 
 @library(scope='TEST SUITE', version='1.2.3', method_dissabler=False)
-class DisablePublicMethods(object):
+class LibraryDecorator(object):
 
     def __init__(self):
         self.invalid = 'This method is not a keyword.'
 
-    def public_method_is_not_keyword(self):
+    def library_decorator_disables_public_methods(self):
         print(self.invalid)
 
-    @keyword(name="Decorated Method Is Not Keyword")
+    @keyword(name="Decorated Method Is Keyword")
     def decorated_method(self):
         print(self.invalid)
 
-    @keyword
-    def _private_method_is_not_keyword(self):
-        print(self.invalid)
